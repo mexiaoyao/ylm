667b56c1bfe0badc15358295ae0f8ed169dca01f
zhenglei <mexiaoyao@163.com>
2023-03-10 22:19:53 +0800

    springboot 初始化

---
+++ src/main/java/com/java/ylm/YlmApplication.java
@@ -0,0 +1,13 @@
+package com.java.ylm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/ylm/YlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ylm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
